cd1818641fd3c1c17afd9af815ae205f663aa9b2
Edremis <algona91@gmail.com>
2023-12-13 01:38:08 +0100

    Addes color change gradually

--- script.js
+++ script.js
@@ -15,9 +15,12 @@ for(let i = 0; i<(boxesPerSide*boxesPerSide);i++){
 for(let i = 0; i<(boxesPerSide*boxesPerSide);i++){
     let a = "div"+i;
     const divdiv = document.getElementById(a);
+    divdiv.counter = "0";
     divdiv.addEventListener("mouseover",()=>{
-        divdiv.classList.add("blue");
+        
+        addColor(divdiv);
     });
+    
 }
 
 changeButton.addEventListener("click",function(){
@@ -53,8 +56,10 @@ function rebuildBox(numberPerSide){
     for(let i = 0; i<(boxesPerSide*boxesPerSide);i++){
         let a = "div"+i;
         const divdiv = document.getElementById(a);
+        divdiv.counter = "0";
         divdiv.addEventListener("mouseover",()=>{
-            divdiv.classList.add("blue");
+            
+            addColor(divdiv);
         });
         let flexValue = 100/boxesPerSide;
         divdiv.style.flex = "1 0 "+flexValue+"%";
@@ -66,6 +71,76 @@ function rebuildBox(numberPerSide){
 function resetBox(){
     let table = container.childNodes;
     for(let i = 0;i < table.length;i++){
-        table[i].classList.remove("blue");
+        table[i].style.backgroundColor = "RGB(255,255,255)";
+        table[i].counter = "0";
+    }
+}
+
+function addColor(elem){
+    console.log(elem.counter);
+    let counter2;
+    switch(elem.counter){
+        case "0":
+            elem.style.backgroundColor = "#999999";
+            counter2 = parseInt(elem.counter);
+            counter2 += 1;
+            elem.counter = counter2.toString();
+            break;
+        case "1":
+            elem.style.backgroundColor = "#888888";
+            counter2 = parseInt(elem.counter);
+            counter2 += 1;
+            elem.counter = counter2.toString();
+            break;
+        case "2":
+            elem.style.backgroundColor = "#777777";
+            counter2 = parseInt(elem.counter);
+            counter2 += 1;
+            elem.counter = counter2.toString();
+            break;
+        case "3":
+            elem.style.backgroundColor = "#666666";
+            counter2 = parseInt(elem.counter);
+            counter2 += 1;
+            elem.counter = counter2.toString();
+            break;
+        case "4":
+            elem.style.backgroundColor = "#555555";
+            counter2 = parseInt(elem.counter);
+            counter2 += 1;
+            elem.counter = counter2.toString();
+            break;
+        case "5":
+            elem.style.backgroundColor = "#444444";
+            counter2 = parseInt(elem.counter);
+            counter2 += 1;
+            elem.counter = counter2.toString();
+            break;
+        case "6":
+            elem.style.backgroundColor = "#333333";
+            counter2 = parseInt(elem.counter);
+            counter2 += 1;
+            elem.counter = counter2.toString();
+            break;
+        case "7":
+            elem.style.backgroundColor = "#222222";
+            counter2 = parseInt(elem.counter);
+            counter2 += 1;
+            elem.counter = counter2.toString();
+            break;
+        case "8":
+            elem.style.backgroundColor = "#111111";
+            counter2 = parseInt(elem.counter);
+            counter2 += 1;
+            elem.counter = counter2.toString();
+            break;
+        case "9":
+            elem.style.backgroundColor = "#000000";
+            counter2 = parseInt(elem.counter);
+            counter2 += 1;
+            elem.counter = counter2.toString();
+            break;
+        case "10":
+            break;
     }
 }
